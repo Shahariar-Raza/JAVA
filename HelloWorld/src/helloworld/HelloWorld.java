75280decd1d317d40c74ff3901dca862e727e8c9
Shaha <Shaha@DESKTOP-BVMJNJ9>
2019-02-14 13:49:54 +0600

    Hello world program in Java

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hi Shahariar");
         System.out.println("This is the first code for git upload");
     }
    
